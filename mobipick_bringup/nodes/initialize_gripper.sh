62f6a57e4e7178d97beda4b0bb762ce9dd74913a
Nils Niemann <nils.niemann@dfki.de>
2019-05-10 11:01:16 +0200

    initialize_gripper: wait for a gripper status message

--- mobipick_bringup/nodes/initialize_gripper.sh
+++ mobipick_bringup/nodes/initialize_gripper.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# wait for the gripper driver to come up, by waiting for a gripper status message
+# ("gripper_hw/input" is input the node gets from the gripper, "output" are commands send to the gripper)
+rostopic echo -n 1 /mobipick/gripper_hw/input
+
 # reset the gripper
 rostopic pub --once /mobipick/gripper_hw/output robotiq_2f_gripper_control/Robotiq2FGripper_robot_output "{rACT: 0, rGTO: 0, rATR: 0, rPR: 0, rSP: 0, rFR: 0}"
 
