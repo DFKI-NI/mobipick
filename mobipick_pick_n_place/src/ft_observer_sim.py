52edd17186ca119465c8e07e8a4bebbbd408ca6a
Christoph Krause <jan_christoph.krause@dfki.de>
2020-10-14 15:17:01 +0200

    substitute for ft observer in simulation with timer

--- mobipick_pick_n_place/src/ft_observer_sim.py
+++ mobipick_pick_n_place/src/ft_observer_sim.py
@@ -17,41 +17,18 @@ class ForceTorqueObserver(object):
     _timer_is_running=False
 
     def __init__(self, name):
-        rospy.wait_for_service('/mobipick/ft_sensor/sensor_acc')
-        rospy.loginfo("connected to sensor accessor service")    
-        rospy.Subscriber("/mobipick/ft_sensor/wrench", WrenchStamped, self.wrench_cb)
-        rospy.loginfo("Subscribed to ft_sensor/wrench")
         self._action_name = name
         self._as = actionlib.SimpleActionServer(self._action_name, FtObserverAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
-      
-    def execute_cb(self, goal):
-        success = False
-        rospy.loginfo("New goal, try to set ft sensor to zero!")
-        try:
-            sensor_acc = rospy.ServiceProxy('/mobipick/ft_sensor/sensor_acc', sensor_accessor)
-            acc_srv = sensor_accessorRequest()
-            acc_srv.command_id=8
-            respond = sensor_acc(acc_srv)
-            if respond.success:
-                rospy.loginfo("ft sensor set to zero")
-                wait_for_detection = True
-                rospy.sleep(5)
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+        rospy.loginfo("ft observer action server in simulation mode started")
         
-        # helper variables
+    def execute_cb(self, goal):
         r = rospy.Rate(1)
+        success = False
         rospy.Timer(goal.timeout, self.timer_cb, True)
         self._timer_is_running = True
         # start executing the action
-        while wait_for_detection and self._timer_is_running:
-            if self._actual_force > goal.threshold:
-                success=True
-                wait_for_detection = False
-                self._feedback.actualForce=self._actual_force
-            rospy.loginfo("actual force: %f" % self._actual_force)
-
+        while self._timer_is_running:    
             # check that preempt has not been requested by the client
             if self._as.is_preempt_requested():
                 rospy.loginfo('%s: Preempted' % self._action_name)
@@ -68,10 +45,6 @@ class ForceTorqueObserver(object):
             rospy.loginfo('%s: Succeeded' % self._action_name)
 
             self._as.set_succeeded(self._result)
-
-    def wrench_cb(self, data):
-        self._wrench = data.wrench
-        self._actual_force = data.wrench.force.y
 
     def timer_cb(self, time):
         self._timer_is_running=False
