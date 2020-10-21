42852346ad5283541a707787d53f06fb3f57bdbb
Christoph Krause <jan_christoph.krause@dfki.de>
2020-10-21 09:56:36 +0200

    New object roof

--- mobipick_pick_n_place/include/mobipick_pick_n_place/fake_object_recognition.h
+++ mobipick_pick_n_place/include/mobipick_pick_n_place/fake_object_recognition.h
@@ -5,7 +5,7 @@
 
 enum ObjectID
 {
-  GETRIEBELAGER = 1, POWER_DRILL = 2, TABLE = 100, COKE_CAN = 101
+  GETRIEBELAGER = 1, POWER_DRILL = 2, TABLE = 100, COKE_CAN = 101, ROOF = 10
 };
 
 #endif //MOBIPICK_PICK_N_PLACE_FAKE_OBJECT_RECOGNITION_H
@@ -22,6 +22,8 @@ std::string id_to_string(int id)
       return "table";
     case ObjectID::COKE_CAN:
       return "coke_can";
+    case ObjectID::ROOF:
+      return "roof";
     default:
       std::cerr << "No such ObjectID: " << id << std::endl;
       return "";
