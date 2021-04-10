cfb19aebb5586046e7bb6ab7a2e77f59109a9c82
Martin Günther <martin.guenther@dfki.de>
2021-04-10 11:03:03 +0200

    Move ccache to gitlab-ci.yml
    This stuff should be kept out of the build.sh, because build.sh can also
    be run locally (where CI_PROJECT_DIR is not defined).

--- build.sh
+++ build.sh
@@ -6,20 +6,9 @@ set -e
 
 export ROS_DISTRO="noetic"
 
-export PATH="/usr/lib/ccache:$PATH"
-export CCACHE_DIR=${CI_PROJECT_DIR}/.ccache
-
-echo "================= ccache stats (before build) ================="
-ccache -s
-echo "==============================================================="
-
 # build all packages in the catkin workspace
 cd ..   # now in catkin_ws/
 source /opt/ros/${ROS_DISTRO}/setup.bash
 catkin init
 catkin config --install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebugInfo
 catkin build --limit-status-rate 0.1 --no-notify
-
-echo "================= ccache stats (after build) =================="
-ccache -s
-echo "==============================================================="
