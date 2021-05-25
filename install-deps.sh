3a48264be734c5813f5b3051acd7961f3ff953d0
Martin Günther <martin.guenther@dfki.de>
2021-05-25 15:36:07 +0200

    Remove trac_ik source dependency
    trac_ik is now available as a binary package on Noetic.

--- install-deps.sh
+++ install-deps.sh
@@ -18,10 +18,9 @@ wstool update
 
 # use rosdep to install all dependencies (including ROS itself)
 sudo apt-get install -qq -y libuvc-dev                          # for astra_camera
-sudo apt-get install -qq -y libnlopt-cxx-dev liborocos-kdl-dev  # for trac_ik_lib, see https://bitbucket.org/traclabs/trac_ik/pull-requests/29#comment-206183885
 sudo apt-get install -qq -y python3-rosdep
 sudo rosdep init > /dev/null 2>&1 || true
 rosdep update
-rosdep install --from-paths ./ -i -y --rosdistro ${ROS_DISTRO} --skip-keys=libuvc --skip-keys=orocos_kdl  # orocos_kdl must be skipped, see https://bitbucket.org/traclabs/trac_ik/pull-requests/29#comment-206183885
+rosdep install --from-paths ./ -i -y --rosdistro ${ROS_DISTRO} --skip-keys=libuvc
 sudo apt-get install -qq -y python3-catkin-tools python3-osrf-pycommon build-essential  # python3-osrf-pycommon has to be installed manually for python3-catkin-tools to work (see https://github.com/catkin/catkin_tools/issues/594)
 sudo apt-get install -qq -y ccache
