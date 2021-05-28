ee2338ecd16bd4cb3c3971ed3435ca52637a597b
Martin Günther <martin.guenther@dfki.de>
2021-05-28 09:54:42 +0200

    gazebo: Add powerdrill_with_grip, use in Moelk demo
    This model corresponds to the real object we use in the physical demo,
    so the parameters for grasping are the same, and the power drill should
    slip less from the gripper.

--- mobipick_pick_n_place/src/gazebo_object_publisher.cpp
+++ mobipick_pick_n_place/src/gazebo_object_publisher.cpp
@@ -9,8 +9,7 @@ std::shared_ptr<ros::Publisher> detection_pub;
 std::shared_ptr<ros::Publisher> pose_power_drill_pub;
 std::shared_ptr<ros::Publisher> pose_table_pub;
 
-
-void gazebo_cb(const gazebo_msgs::LinkStatesConstPtr msg)
+void gazebo_cb(const gazebo_msgs::LinkStatesConstPtr &msg)
 {
   // input topic has 1000 Hz, output topic should have 10 Hz
   static size_t counter = 0;
@@ -33,7 +32,7 @@ void gazebo_cb(const gazebo_msgs::LinkStatesConstPtr msg)
       // TODO: Change to tf listener?
       if (msg->name[i] == "mobipick::mobipick/base_footprint" && !base_link_found)
       {
-        base_link_found= true;
+        base_link_found = true;
         tf::poseMsgToEigen(msg->pose[i], mobipick_pose);
         mobipick_pose = mobipick_pose.inverse();
       }
@@ -52,7 +51,7 @@ void gazebo_cb(const gazebo_msgs::LinkStatesConstPtr msg)
           Eigen::Isometry3d object_pose;
           tf::poseMsgToEigen(msg->pose[i], object_pose);
           Eigen::Isometry3d object_to_bbox = Eigen::Isometry3d::Identity();
-          object_to_bbox.translation() = Eigen::Vector3d(0.0d, 0.0d, det3d.bbox.size.z / 2.0d);
+          object_to_bbox.translation() = Eigen::Vector3d(0.0, 0.0, det3d.bbox.size.z / 2.0);
           tf::poseEigenToMsg((mobipick_pose * object_pose * object_to_bbox), det3d.bbox.center);
 
           det3d.results.resize(1);
@@ -65,8 +64,7 @@ void gazebo_cb(const gazebo_msgs::LinkStatesConstPtr msg)
           pose_msg.header = det3d.header;
           pose_msg.pose = det3d.bbox.center;
           pose_table_pub->publish(pose_msg);
-        }
-        if (msg->name[i] == "cokecan_1::coke_can")
+        } else if (msg->name[i] == "cokecan_1::coke_can")
         {
           // add coke_can
           vision_msgs::Detection3D det3d;
@@ -79,7 +77,7 @@ void gazebo_cb(const gazebo_msgs::LinkStatesConstPtr msg)
           Eigen::Isometry3d object_pose;
           tf::poseMsgToEigen(msg->pose[i], object_pose);
           Eigen::Isometry3d object_to_bbox = Eigen::Isometry3d::Identity();
-          object_to_bbox.translation() = Eigen::Vector3d(0.0d, 0.0d, det3d.bbox.size.z / 2.0d);
+          object_to_bbox.translation() = Eigen::Vector3d(0.0, 0.0, det3d.bbox.size.z / 2.0);
           tf::poseEigenToMsg((mobipick_pose * object_pose * object_to_bbox), det3d.bbox.center);
 
           det3d.results.resize(1);
@@ -87,10 +85,8 @@ void gazebo_cb(const gazebo_msgs::LinkStatesConstPtr msg)
           tf::poseEigenToMsg((mobipick_pose * object_pose), det3d.results[0].pose.pose);
           det3d.results[0].score = 1.0;
           detections.detections.push_back(det3d);
-        }
-        if (msg->name[i] == "powerdrill_1::powerdrill")
+        } else if (msg->name[i] == "powerdrill_1::powerdrill")
         {
-          
           // add powerdrill
           vision_msgs::Detection3D det3d;
           det3d.header = detections.header;
@@ -103,11 +99,42 @@ void gazebo_cb(const gazebo_msgs::LinkStatesConstPtr msg)
           tf::poseMsgToEigen(msg->pose[i], object_pose);
 
           Eigen::Isometry3d object_to_bbox = Eigen::Isometry3d::Identity();
-          object_to_bbox.translation() = Eigen::Vector3d(-0.046d, 0.01055d, 0.02545d);
+          object_to_bbox.translation() = Eigen::Vector3d(-0.046, 0.01055, 0.02545);
 
           // rotate to dope orientation
           Eigen::Isometry3d bbox_center_rotated = Eigen::Isometry3d::Identity();
-          bbox_center_rotated.rotate(Eigen::AngleAxisd(M_PI, Eigen::Vector3d(1.0d, 0.0d, 0.0d)));
+          bbox_center_rotated.rotate(Eigen::AngleAxisd(M_PI, Eigen::Vector3d(1.0, 0.0, 0.0)));
+          tf::poseEigenToMsg((mobipick_pose * object_pose * object_to_bbox * bbox_center_rotated), det3d.bbox.center);
+
+          det3d.results.resize(1);
+          det3d.results[0].id = ObjectID::POWER_DRILL;
+          tf::poseEigenToMsg((mobipick_pose * object_pose * bbox_center_rotated), det3d.results[0].pose.pose);
+          det3d.results[0].score = 1.0;
+          detections.detections.push_back(det3d);
+          // publish power drill pose seperately
+          geometry_msgs::PoseStamped pose_msg;
+          pose_msg.header = det3d.header;
+          pose_msg.pose = det3d.bbox.center;
+          pose_power_drill_pub->publish(pose_msg);
+        } else if (msg->name[i] == "powerdrill_with_grip_1::powerdrill_with_grip")
+        {
+          // add powerdrill
+          vision_msgs::Detection3D det3d;
+          det3d.header = detections.header;
+          det3d.bbox.size.x = 0.184208;
+          det3d.bbox.size.y = 0.187514;
+          det3d.bbox.size.z = 0.057294;
+
+          // shift to center of bbox and rotate coordinate frames to match dope detection
+          Eigen::Isometry3d object_pose;
+          tf::poseMsgToEigen(msg->pose[i], object_pose);
+
+          Eigen::Isometry3d object_to_bbox = Eigen::Isometry3d::Identity();
+          object_to_bbox.translation() = Eigen::Vector3d(0.0, 0.0, 0.0);
+
+          // rotate to dope orientation
+          Eigen::Isometry3d bbox_center_rotated = Eigen::Isometry3d::Identity();
+          bbox_center_rotated.rotate(Eigen::AngleAxisd(M_PI, Eigen::Vector3d(1.0, 0.0, 0.0)));
           tf::poseEigenToMsg((mobipick_pose * object_pose * object_to_bbox * bbox_center_rotated), det3d.bbox.center);
 
           det3d.results.resize(1);
@@ -131,9 +158,12 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "gazebo_object_publisher");
   ros::NodeHandle nh;
-  detection_pub = std::make_shared<ros::Publisher>(nh.advertise<vision_msgs::Detection3DArray>("dope/detected_objects", 10));
-  pose_power_drill_pub = std::make_shared<ros::Publisher>(nh.advertise<geometry_msgs::PoseStamped>("/mobipick/dope/pose_power_drill_with_grip", 10));
-  pose_table_pub = std::make_shared<ros::Publisher>(nh.advertise<geometry_msgs::PoseStamped>("/mobipick/dope/pose_table", 10));
+  detection_pub =
+      std::make_shared<ros::Publisher>(nh.advertise<vision_msgs::Detection3DArray>("dope/detected_objects", 10));
+  pose_power_drill_pub = std::make_shared<ros::Publisher>(nh.advertise<geometry_msgs::PoseStamped>(
+      "/mobipick/dope/pose_power_drill_with_grip", 10));
+  pose_table_pub =
+      std::make_shared<ros::Publisher>(nh.advertise<geometry_msgs::PoseStamped>("/mobipick/dope/pose_table", 10));
   ros::Subscriber sub = nh.subscribe("/gazebo/link_states", 10, gazebo_cb);
 
   ros::spin();
