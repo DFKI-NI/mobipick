9026886aa453a2adb66776672551a62131d467e6
Martin Günther <martin.guenther@dfki.de>
2021-06-03 10:52:40 +0200

    Modify build.sh to make it easier for shellcheck

--- build.sh
+++ build.sh
@@ -4,11 +4,9 @@ set -e
 # This script must be run in the in catkin_ws/src/ directory, i.e., the parent
 # directory of this repo.
 
-export ROS_DISTRO="noetic"
-
 # build all packages in the catkin workspace
 cd ..   # now in catkin_ws/
-source /opt/ros/${ROS_DISTRO}/setup.bash
+source /opt/ros/noetic/setup.bash
 catkin init
 catkin config --install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebugInfo
 catkin build --limit-status-rate 0.1 --no-notify
