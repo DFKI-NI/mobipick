0210868fa2d2f6f6d7e1ec633e095255707ae47d
Martin Günther <martin.guenther@dfki.de>
2024-08-13 15:13:04 +0200

    Update mdl style config
    * don't exclude fenced-code-language
    * update code_blocks -> ignore_code_blocks (code_blocks is deprecated in
      mdl 0.13)
    * use rule names instead of numbers

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -2,23 +2,23 @@
 
 all
 
-exclude_rule 'fenced-code-language' # Fenced code blocks should have a language specified
+#exclude_rule 'fenced-code-language' # Fenced code blocks should have a language specified
 exclude_rule 'first-line-h1' # First line in file should be a top level header
-
+#exclude_rule 'first-header-h1'
 exclude_rule 'ul-style'
-
 exclude_rule 'no-multiple-blanks'
-
 exclude_rule 'header-style'
+#exclude_rule 'no-bare-urls'
+#exclude_rule 'no-inline-html'
 
-# Line lenght
-rule 'MD013', :line_length => 120, :code_blocks => false, :tables => false
+# Line length
+rule 'line-length', :line_length => 120, :ignore_code_blocks => true, :tables => false
 
 # Unordered list indentation
-rule 'MD007', :indent => 2
+rule 'ul-indent', :indent => 2
 
 # Ordered list item prefix
-rule 'MD029', :style => 'ordered'
+rule 'ol-prefix', :style => 'ordered'
 
 # Multiple headers with the same content
 rule 'no-duplicate-header', :allow_different_nesting => true
